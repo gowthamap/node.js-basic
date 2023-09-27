772af18071421aee8fbfa9ddd7f723cc706aa36f
Gowthamap <gowthamap55555@gmail.com>
2023-09-27 23:25:38 +0530

    feat: module.exports in node.js

--- script.js
+++ script.js
@@ -1,34 +1,4 @@
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
+const { logger, myName } = require("./utils");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(
-      path.join(__dirname, "views", "index.html"),
-      "utf8",
-      (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    );
-  } 
-  
-  if (req.url === "/contact") {
-    fs.readFile(
-      path.join(__dirname, "views", "contact.html"),
-      "utf8",
-      (err, data) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      }
-    );
-  } 
-
-});
-
-const PORT = process.env.PORT || 3001;
-
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+console.log(logger());
+console.log(myName);
---
+++ utils.js
@@ -0,0 +1,10 @@
+const logger = () => {
+  return 2 * 5;
+};
+
+const myName = "Debug Media";
+
+module.exports = {
+  logger,
+  myName,
+};
