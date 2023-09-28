017c489b36f73c0740ab015e1badef6ddce0f807
Gowthamap <gowthamap55555@gmail.com>
2023-09-28 11:38:44 +0530

    feat: express basics

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-const { logger, myName } = require("./utils");
+const express = require("express");
+const app = express();
 
-console.log(logger());
-console.log(myName);
+
+app.use(express.static("views"))
+
+
+app.get("*", (req,res)=>{
+  res.status(404).send("<h1>404</h1>")
+})
+
+
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
