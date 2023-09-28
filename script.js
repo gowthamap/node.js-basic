d6c24c1a64a0514e526569194a3f5fac70517797
Gowthamap <gowthamap55555@gmail.com>
2023-09-28 18:20:38 +0530

    feat: basic login form

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 const express = require("express");
 const app = express();
-const Token = require("./middleware/token");
-const Validation = require("./middleware/validation");
 
-app.use(DateGenerator);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("views"));
 
-const middleware = [Token, Validation];
+const emailDB = "debugmedia@gmail.com";
+const passwordDB = "123";
 
-app.get("/profile", [...middleware], (req, res) => {
-  console.log("User Logged");
-  res.send("<h1>Success</h1>");
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  if (email === emailDB && password === passwordDB) {
+    res.send("Login Successful");
+  } else {
+    res.send("Login failed");
+  }
 });
-
-app.get("/", (req, res) => {
-  console.log("Default URL");
-  res.send("<h1>Home</h1>");
-});
-
-function DateGenerator(req, res, next) {
-  console.log(new Date());
-  next();
-}
 
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT);
