e80c806ffc2d116a66fb3af437cda9033c718733
Gowthamap <gowthamap55555@gmail.com>
2023-09-30 14:44:12 +0530

    feat: postman and node js query params and path params

--- script.js
+++ script.js
@@ -3,19 +3,40 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("views"));
 
-const emailDB = "debugmedia@gmail.com";
-const passwordDB = "123";
+const userData = [
+  { id: 1, name: "debug", company: "Google" },
+  { id: 2, name: "media", company: "Facebook" },
+  { id: 3, name: "sanjay", company: "Microsoft" },
+  { id: 3, name: "rahul", company: "Microsoft" },
+];
 
-app.post("/login", (req, res) => {
-  const { email, password } = req.body;
+app.post("/user", (req, res) => {
+  const query = parseInt(req.query.id);
+  const userName = req.query.name;
 
-  if (email === emailDB && password === passwordDB) {
-    res.send("Login Successful");
-  } else {
-    res.send("Login failed");
-  }
+  const filteredUserData = userData.filter((data) => {
+    if (query && userName) {
+      return data.id === query && data.name === userName;
+    } else {
+      return data;
+    }
+  });
+
+  res.json(filteredUserData);
+});
+
+app.post("/user/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const userName = req.params.name;
+
+  const filteredUserData = userData.filter((data) => data.id === userId);
+
+  res.json(filteredUserData);
+});
+
+app.get("*", (req, res) => {
+  res.json("No Route Available");
 });
 
 const PORT = process.env.PORT || 3001;
